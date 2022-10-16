dd2ab4f1d77ccf37575f4afa3603af17edf769ce
gilsan <jhhan001@gmail.com>
2022-10-16 10:43:52 +0900

    commit

---
+++ src/app/ngcomponent100/pipes/filter-term.pipe.ts
@@ -0,0 +1,15 @@
+
+import { Pipe, PipeTransform } from '@angular/core';
+
+
+@Pipe({
+  name: 'appFilterTerm'
+})
+export class FilterTermPipe implements PipeTransform {
+  transform(items:string[],  searchTerm: string = ''): string[] {
+     return items.filter((item) => {
+        item.toLowerCase().includes(searchTerm.toLowerCase())
+     });
+  }
+
+}
