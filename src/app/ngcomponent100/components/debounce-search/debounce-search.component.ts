a9cd0cee963dcfab3fc1dec653caa73006616db2
gilsan <jhhan001@gmail.com>
2022-10-18 11:16:21 +0900

    commit

---
+++ src/app/ngcomponent100/components/debounce-search/debounce-search.component.ts
@@ -0,0 +1,11 @@
+import { Component, Output, EventEmitter, Input } from '@angular/core';
+
+
+@Component({
+  selector: 'app-debounce-search',
+  templateUrl: './debounce-search.component.html',
+  styleUrls: ['./debounce-search.component.scss']
+})
+export class DebounceSearchComponent {
+
+}
