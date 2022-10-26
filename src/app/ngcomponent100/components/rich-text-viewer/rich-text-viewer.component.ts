0605f9e3e769e6300aebd271d25efc79c9d8394a
gilsan <jhhan001@gmail.com>
2022-10-26 12:00:09 +0900

    commit

--- src/app/ngcomponent100/components/rich-text-viewer/rich-text-viewer.component.ts
+++ src/app/ngcomponent100/components/rich-text-viewer/rich-text-viewer.component.ts
@@ -1,12 +1,16 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: 'app-rich-text-viewer',
   templateUrl: './rich-text-viewer.component.html',
   styleUrls: ['./rich-text-viewer.component.scss']
 })
-export class RichTextViewerComponent {
+export class RichTextViewerComponent  implements OnInit{
 
   constructor() {}
+
+  ngOnInit(): void {
+
+  }
 
 }
