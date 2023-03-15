a2502636b95bd956aaa0210fea11d2e33eab67b2
gilsan <jhhan001@gmail.com>
2023-03-16 08:33:47 +0900

    commit

--- src/app/gangwon/components/restorant/restorant.component.ts
+++ src/app/gangwon/components/restorant/restorant.component.ts
@@ -10,7 +10,8 @@ export class RestorantComponent implements OnInit {
 
 
   ngOnInit() {
-    console.log('tag ===== ==== ======== == gjcity/gjcity123', );
+    let test ="hong";
+    console.log('tag ===== ==== ======= == gjcity/gjcity123', );
 
   }
 
